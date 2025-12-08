1cb81fee7671c631805ab249cb2957d7ec909b4c
francis-bunker <185110850+francis-bunker@users.noreply.github.com>
2025-12-07 21:28:20 -0500

    fix 3

--- index.js
+++ index.js
@@ -7,50 +7,34 @@ import db from "./Database/index.js";
 import mongoose from "mongoose";
 import Users from "./Users/routes.js";
 import MapRoutes from "./maps/routes.js";
-import MongoStore from "connect-mongo";
 
-const CONNECTION_STRING =
-  process.env.DATABASE_CONNECTION_STRING ||
-  "mongodb://127.0.0.1:27017/sm";
-
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/sm"
 mongoose.connect(CONNECTION_STRING);
 
-const app = express();
-
-
+const app = express()
 app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
-  })
+    cors({
+        credentials: true,
+        origin: process.env.CLIENT_URL || "http://localhost:3000",
+    })
 );
-
 const sessionOptions = {
-  secret: process.env.SESSION_SECRET || "sm",
+  secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
-  proxy: true,
-
-  store: MongoStore.create({
-    mongoUrl: CONNECTION_STRING,
-  }),
-
-  cookie: {
+};
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
     domain: process.env.SERVER_URL,
-  },
-};
-
+  };
+}
 app.use(session(sessionOptions));
 app.use(express.json());
-
 Posts(app, db);
 Users(app);
 MapRoutes(app);
-
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(4000);
+app.get('/hello', (req, res) => { res.send('Hello World!') })
+app.listen(4000)
